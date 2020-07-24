1bb293995c2d39e9772c1789af34b49574548004
Cooper Sheets <coopersheets@gmail.com>
2020-07-24 13:54:14 -0500

    Created the  Shapes project and the base Shape class.

---
+++ Shapes/Shape.cpp
@@ -0,0 +1,17 @@
+#include "Shape.h"
+
+Shape::Shape()
+{
+	area = 0;
+	perimeter = 0;
+}
+
+float Shape::Area()
+{
+	return area;
+}
+
+float Shape::Perimeter()
+{
+	return perimeter;
+}
---
+++ Shapes/Shape.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Base class
+class Shape
+{
+private:
+	float area;
+	float perimeter;
+
+public:
+	Shape();
+	float Area();
+	float Perimeter();
+};
